32b4999797908956db155a237fff3482bc980899
k456 <github.com@amrps.net>
2012-07-12 18:32:58 +0900

    install.sh bug fix

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-orig_dir=`pwd`
-cd `dirname $0`
+dotfiles_dir=`dirname $0`
 git submodule init
 git submodule update
-ln -s _vimrc ~/.vimrc
-ln -s vimfiles ~/.vim
-cd $orig_dir
+ln -s ${dotfiles_dir}/_vimrc ~/.vimrc
+ln -s ${dotfiles_dir}/vimfiles ~/.vim
+ln -s ${dotfiles_dir}/_ctags ~/.ctags
