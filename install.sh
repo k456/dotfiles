c35f41adb7225c138cf4685f152393e6076e4623
k456 <github.com@amrps.net>
2012-07-13 23:50:07 +0900

    install.sh bug fix

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 orig_dir=`pwd`
-dotfiles_dir=`dirname $0`
+dotfiles_dir=$(cd $(dirname $0) && pwd)
 cd ${dotfiles_dir}
 git submodule init
 git submodule update
-cd ${orig_dir}
 ln -s ${dotfiles_dir}/_vimrc ~/.vimrc
 ln -s ${dotfiles_dir}/vimfiles ~/.vim
 ln -s ${dotfiles_dir}/_ctags ~/.ctags
+cd ${orig_dir}
 vim +BundleInstall +qall
